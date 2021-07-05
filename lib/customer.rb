32cc4066dac36b25c4be3728f067bc3fe84f3506
Alberto <ajsaenz1091@gmail.com>
2021-07-05 13:09:32 -0400

    commit tree e848d45e39fc853fc08771dc4e4114f8f938b002

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,33 @@
 class Customer
-  
+    attr_accessor 
+    attr_reader :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    # initializes a meal using the current Customer instance, a provided Waiter instance and a total and tip
+    def new_meal(waiter,total,tip)
+        meal = Meal.new(waiter,self,total,tip)
+    end
+
+    # returns an Array of Meal instances associated with this customer
+    def meals
+        meals = Meal.all.filter{|meal| meal.customer == self}
+    end
+
+    # returns an Array of Waiter instances associated with this customer's meals
+
+    def waiters
+        self.meals.map{|meal| meal.waiter }
+    end
+
+    def self.all
+        @@all
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,33 @@
 class Waiter
+    attr_accessor :experience
+    attr_reader :name
 
+    @@all = []
+
+    def initialize(name, experience)
+        @name = name
+        @experience = experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    # initializes a meal using the current Waiter instance, a provided Customer instance and a total and tip
+    def new_meal(customer, total, tip)
+        meal = Meal.new(self, customer, total,tip)
+    end
+
+    # meals returns an Array of Meal instances associated with this waiter
+
+    def meals
+        meals = Meal.all.filter{|meal| meal.waiter == self}
+    end
+
+    # returns the Customer instance associated with the meal that received the largest tip
+
+    def best_tipper
+        best_tipper = self.meals.max_by(&:tip).customer
+    end
 end
